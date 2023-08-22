6e8a06a820755ea30a72e8ec7f2501baacea2af7
Daniel Baranowski <daniel.baranowski@infinityworks.com>
2023-08-22 23:37:51 +0100

    updated hanko

--- backend/handler/user.go
+++ backend/handler/user.go
@@ -287,11 +287,10 @@ func (h *UserHandler) Logout(c echo.Context) error {
 	}
 
 	cookie, err := h.sessionManager.DeleteCookie()
-	if err != nil {
-		return fmt.Errorf("failed to create session token: %w", err)
+	if err == nil {
+		c.SetCookie(cookie)
+	} else {
 	}
-
-	c.SetCookie(cookie)
 
 	return c.NoContent(http.StatusNoContent)
 }
